5a67a32da4391024f702c2416aa6b69c8a7f272e
Kevin Lin <kevin@lin.dev>
2022-04-08 17:28:46 -0700

    Added connect wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './styles/App.css';
 
@@ -7,6 +7,67 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask -> https://metamask.io/');
+        return;
+      }
+
+      // Fancy method to request access to account.
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+      // Boom! This should print out public address once we authorize Metamask.
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Gotta make sure this is async.
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    // Check if we're authorized to access the user's wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // Users can have multiple authorized accounts, we grab the first one if its there!
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  // Create a function to render if wallet is not connected yet
+  const renderNotConnectedContainer = () => (
+    <div className="connect-wallet-container">
+      <video src="/crew_lvl32_nft.mp4" type="video/mp4" autoPlay muted loop />
+      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+        Connect Wallet
+      </button>
+    </div>
+  );
+
+  // This runs our function when the page loads.
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -18,6 +79,9 @@ const App = () => {
             </div>
           </header>
         </div>
+
+        {/* Hide the connect button if currentAccount isn't empty*/}
+        {!currentAccount && renderNotConnectedContainer()}
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
