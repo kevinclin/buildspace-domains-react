dee050ea83caf7299c7a1f026f350cd9245b8187
Kevin Lin <kevin@lin.dev>
2022-04-11 17:26:01 -0700

    Change wrong address

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import contractAbi from './utils/contractABI.json';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const tld = '.crew';
-const CONTRACT_ADDRESS = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState('');
