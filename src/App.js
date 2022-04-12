b5cad272ebbbbe3ac3fed5d7d55b6ded270e9ae2
Kevin Lin <kevin@lin.dev>
2022-04-11 19:29:17 -0700

    Update placeholder

--- src/App.js
+++ src/App.js
@@ -228,7 +228,7 @@ const App = () => {
         <input
           type="text"
           value={record}
-          placeholder="whats ur ninja power?"
+          placeholder="whats ur fav artist?"
           onChange={(e) => setRecord(e.target.value)}
         />
         {/* If the editing variable is true, return the "Set record" and "Cancel" button */}
